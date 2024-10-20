9d58d4470671d61096c5da89319d6bc183bd339b
aernest njuki <74731728+Aernestnjuki@users.noreply.github.com>
2024-10-21 01:09:46 +0300

    Created a custorm user model, user_profile model and set up the setting,py file

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, User_Profile
 
-# Register your models here.
+class User_Admin_modification(admin.ModelAdmin):
+    list_display = ['username', 'email']
+
+class User_Profile_Admin_Modification(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'verified']
+
+admin.site.register(User, User_Admin_modification)
+admin.site.register(User_Profile, User_Profile_Admin_Modification)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, max_length=150)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+    
+class User_Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=300)
+    image = models.FileField(upload_to='user_profile', default='default.jpg')
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.full_name
+    
+@receiver(post_save, sender=User)
+def create_users_profile(sender, created, instance, **kwargs):
+    if created:
+        add_users_profile = User_Profile.objects.create(user=instance)
+        add_users_profile.save()
+    
+
+
